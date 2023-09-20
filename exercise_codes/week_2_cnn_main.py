f3a7195d06a48b27681cef43cf4a0dc366132e6f
naye0319 <naye03@naver.com>
2023-09-21 05:13:53 +0900

    add cnn

---
+++ exercise_codes/week_2_cnn_main.py
@@ -0,0 +1,130 @@
+import numpy as np
+import torch
+import torch.nn as nn
+from keras.datasets import mnist
+import random
+import torch.optim as optim
+from tqdm import tqdm
+import os
+import sys
+
+############## this block is just for import moudles ######
+current_path = os.path.dirname(os.path.realpath(__file__))
+parent_path = os.path.dirname(current_path)
+sys.path.append(parent_path)
+###########################################################
+
+#import custom models
+from exercise_codes.week_2_models import *
+
+def load_data():
+    #load mnist dataset.
+    (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+    #since dataset is too big for cpu, reduce size
+    train_random_indices = random.sample(range(len(train_images)), 30000)
+    test_random_indices = random.sample(range(len(test_images)), 10000)
+
+    #train_images = train_images[train_random_indices]
+    #train_labels = train_labels[train_random_indices]
+
+    #test_images = test_images[test_random_indices]
+    #test_labels = test_labels[test_random_indices]
+
+    #print datset shape
+    print("train image shape:", train_images.shape)
+    print("train label shape:", train_labels.shape)
+    print("test image shape:", test_images.shape)
+    print("test label shape:", test_labels.shape)
+    
+    return train_images, train_labels, test_images, test_labels
+
+def train_th(model, image, label, test_image, test_label, criterion):
+    
+    
+    image = torch.tensor(image, dtype=torch.float32)
+    label = torch.tensor(label, dtype=torch.long)
+
+    data_len = image.size()[0]
+
+    optimizer = optim.SGD(model.parameters(), lr=LR)
+    
+    for epoch in range(TOTAL_EPOCH):
+        epoch_loss = 0.0
+        tqdm_batch = tqdm(range(data_len // BATCH_SIZE), desc=f"Epoch {epoch + 1}/{TOTAL_EPOCH}")
+        for i, batch in enumerate(tqdm_batch):
+            start = batch * BATCH_SIZE
+            end = (batch+1) * BATCH_SIZE
+            
+            image_batch = image[start:end]
+            label_batch = label[start:end]
+            
+            optimizer.zero_grad()
+            
+            output = model(image_batch)
+            
+            loss = criterion(output, label_batch)
+            epoch_loss+=loss.item()
+            
+            loss.backward()
+            
+            optimizer.step()
+            
+            tqdm_batch.set_postfix(loss=epoch_loss/(i+1))
+        
+        accuracy = validate_th(model, test_image, test_label)
+    return accuracy
+
+def validate_th(model, image, label):
+    image = torch.tensor(image, dtype=torch.float32)
+    label = torch.tensor(label, dtype=torch.long)
+    
+    data_len = image.size()[0]
+    
+    total_data = 0
+    total_correct = 0
+    tqdm_batch = tqdm(range(data_len // BATCH_SIZE), desc=f"validation")
+    for i, batch in enumerate(tqdm_batch):
+            start = batch * BATCH_SIZE
+            end = (batch+1) * BATCH_SIZE
+            
+            image_batch = image[start:end]
+            label_batch = label[start:end]
+                       
+            output = model(image_batch)
+            
+            predicted_classes = torch.argmax(output, dim=1)
+            total_correct += (predicted_classes == label_batch).sum().item()
+            total_data += label_batch.size(0)
+            
+            tqdm_batch.set_postfix(accuracy=total_correct/total_data)
+            
+    accuracy = total_correct / total_data
+    return accuracy
+
+def start(train_linear_th=False, train_linear_np=False, train_cnn_th=False, train_cnn_np=False):
+    print("loading...")
+    train_images, train_labels, test_images, test_labels = load_data()
+    print("========================\n\n")
+    
+    if(train_linear_th):
+        print("========================")
+        print("training linear torch model...")
+        model = linear_model_th()
+        accuracy = train_th(model, train_images, train_labels, test_images, test_labels, criterion=nn.CrossEntropyLoss())
+        print(f"training with linear_th accuracy: {accuracy * 100:.2f}%")
+        print("========================")
+    if(train_cnn_th):
+        print("========================")
+        print("training cnn torch model...")
+        model = cnn_model_th()
+        accuracy = train_th(model, train_images, train_labels, test_images, test_labels, criterion=nn.CrossEntropyLoss())
+        print(f"training with cnn_th accuracy: {accuracy * 100:.2f}%")
+        print("========================")
+
+if __name__=="__main__":
+    random.seed(24)
+    TOTAL_EPOCH=5
+    BATCH_SIZE=48
+    LR=0.001
+    start(train_linear_th=True,train_linear_np=False,train_cnn_th=True,train_cnn_np=False)
---
+++ exercise_codes/week_2_models.py
@@ -0,0 +1,102 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import sys
+import os
+
+############## this block is just for import moudles ######
+current_path = os.path.dirname(os.path.realpath(__file__))
+parent_path = os.path.dirname(current_path)
+sys.path.append(parent_path)
+###########################################################
+
+from torch_models.activations.relu import Relu_th
+from numpy_models.activations.relu import Relu_np
+
+from torch_models.activations.sigmoid import Sigmoid_th
+from numpy_models.activations.sigmoid import Sigmoid_np
+
+from torch_models.losses.binary_ce import Binary_Cross_Entropy_th
+from numpy_models.losses.binary_ce import Binary_Cross_Entropy_np
+
+from torch_models.commons.linear import Linear_th
+from numpy_models.commons.linear import Linear_np
+
+
+class linear_model_th(nn.Module):
+    def __init__(self, input_channel=28*28, output_channel=10) -> None:
+        super().__init__()
+        
+        self.linear_1 = Linear_th(input_channel , 256)
+        self.linear_2 = Linear_th(256, 128)
+        self.linear_3 = Linear_th(128, output_channel)
+        self.activation_1 = Relu_th()
+        self.activation_2 = Relu_th()
+        self.sigmoid = Sigmoid_th()
+        
+        
+    def forward(self,x):
+        #make x flatten [# of batch, 28*28 ]
+        batch_size = x.size()[0]
+        x = x.view(batch_size,-1)
+        
+        x = self.linear_1(x)
+        x = self.activation_2(x)
+        x = self.linear_2(x)
+        x = self.activation_2(x)
+        x = self.linear_3(x)
+        x = self.sigmoid(x)
+        
+        return x
+
+class linear_model_np():
+    def __init__(self) -> None:
+        pass
+    def forward(self,x):
+        pass
+    def backward(self):
+        pass
+    def update_grad(self):
+        pass
+
+class cnn_model_th(nn.Module):
+    def __init__(self, input_channel=28*28, output_channel=10) -> None:
+        super().__init__()
+        
+        self.cnn_1 = nn.Conv2d(1 , 8, kernel_size=(3,3), stride=(1,1),padding=(1,1)) #28,28 -> 28,28
+        self.pooling1 = nn.MaxPool2d(kernel_size=(2,2), stride=(2,2)) #28,28 -> 14,14
+        self.cnn_2 = nn.Conv2d(8 , 12, kernel_size=(3,3), stride=(1,1),padding=(1,1)) #14,14 -> 14,14
+        self.pooling2 = nn.MaxPool2d(kernel_size=(2,2), stride=(2,2)) #14,14 -> 7.7
+        
+        self.linear1 = Linear_th( 7*7*12 , 300)
+        self.linear2 = Linear_th(300, output_channel)
+        
+        self.activation_1 = Relu_th()
+        self.activation_2 = Relu_th()
+        self.activation_3 = Relu_th()
+        self.sigmoid = Sigmoid_th()
+        
+    
+    def forward(self,x):
+        #change [# of batch, channel, x_shape, y_shape]
+        batch_size = x.size()[0]
+        x = x.view(-1,1,28,28)
+        
+        #cnn part
+        x = self.cnn_1(x)
+        x = self.activation_1(x)
+        x = self.pooling1(x)
+        x = self.cnn_2(x)
+        x = self.activation_2(x)
+        x = self.pooling2(x)
+        
+        #flatten
+        x = x.view(batch_size, -1)
+        
+        #linear part
+        x = self.linear1(x)
+        x = self.activation_3(x)
+        x = self.linear2(x)
+        x = self.sigmoid(x)
+        
+        return x

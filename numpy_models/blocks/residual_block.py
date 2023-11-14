a51c45c2c176e55aaf2983886e5c014063a0aefe
naye971012 <naye03@naver.com>
2023-11-14 21:17:39 +0900

    add residual_block

---
+++ numpy_models/blocks/residual_block.py
@@ -0,0 +1,17 @@
+from typing import Any
+import numpy as np
+
+class Residual_block_np:
+    def __init__(self, model) -> None:
+        self.model = model
+    
+    def forward(self,x:np.array):
+        out = self.model(x)
+        return out + x
+    
+    def backward(self,d_prev):
+        d_out = self.model.backward(d_prev)
+        return d_prev - d_out
+    
+    def __call__(self,x):
+        return self.forward(x)
---
+++ torch_models/blocks/residual_block.py
@@ -0,0 +1,1 @@
+#it is same as numpy residual block
